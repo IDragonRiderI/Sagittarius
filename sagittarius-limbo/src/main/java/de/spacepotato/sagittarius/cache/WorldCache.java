bcaf66d75202386e74271ade70c82668d2f83b3b
IDragonRiderI <IDragonRiderI@nyron.net>
2023-12-16 15:49:17 +0100

    Make world loading async to improve startup time for large worlds

--- sagittarius-limbo/src/main/java/de/spacepotato/sagittarius/cache/WorldCache.java
+++ sagittarius-limbo/src/main/java/de/spacepotato/sagittarius/cache/WorldCache.java
@@ -2,6 +2,7 @@ package de.spacepotato.sagittarius.cache;
 
 import com.carrotsearch.hppc.cursors.ObjectCursor;
 import de.spacepotato.sagittarius.Sagittarius;
+import de.spacepotato.sagittarius.SagittariusImpl;
 import de.spacepotato.sagittarius.entity.PlayerImpl;
 import de.spacepotato.sagittarius.network.protocol.PacketContainer;
 import de.spacepotato.sagittarius.network.protocol.play.ServerMapChunkBulkPacket;
@@ -17,81 +18,93 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 public class WorldCache {
 
-	private List<PacketContainer> worldPackets;
+	private CompletableFuture<List<PacketContainer>> worldPackets;
 	private final List<WorldLoader> loaders;
+	private final ExecutorService executor;
 	
 	public WorldCache() {
 		loaders = new ArrayList<>();
 		loaders.add(new WorldEditSchematicLoader());
 		loaders.add(new BlankWorldLoader());
+		executor = Executors.newSingleThreadExecutor();
 	}
 	
 	public void load() {
-		WorldImpl world = null;
-		for (WorldLoader loader : loaders) {
-			if (loader.isSupported()) {
-				log.info("Loading world...");
-				world = loader.loadDefaultWorld();
-				break;
+		worldPackets = new CompletableFuture<List<PacketContainer>>();
+		executor.submit(() -> {
+			WorldImpl world = null;
+			for (WorldLoader loader : loaders) {
+				if (loader.isSupported()) {
+					log.info("Loading world...");
+					world = loader.loadDefaultWorld();
+					break;
+				}
 			}
-		}
-		world.expand();
-		world.applyBiome(Sagittarius.getInstance().getConfig().getBiome());
-		
-		List<PacketContainer> worldPackets = new ArrayList<>();
-		
-		List<Chunk> chunks = new ArrayList<>();
-		for (ObjectCursor<ChunkImpl> cursor : world.getChunks().values()) {
-			chunks.add(cursor.value);
-		}
-		
-		boolean skylight = Sagittarius.getInstance().getConfig().getDimension() == Dimension.OVERWORLD;
-		
-		// We try to send as few packets as possible.
-		// if we do end up with a packet that is too big, then we're sending the next chunks in a separate packet.
-		List<Integer> breaks = new ArrayList<>();
-		int totalSize = 0;
-		for (int i = 0; i < chunks.size(); i++) {
-			Chunk chunk = chunks.get(i);
-			totalSize += chunk.getSizeEstimate((short) chunk.calculatePrimaryBitMask(), skylight, true);
-			if (totalSize > 1_900_000) {
-				breaks.add(i);
-				totalSize = 0;
+			world.expand();
+			world.applyBiome(Sagittarius.getInstance().getConfig().getBiome());
+			
+			List<PacketContainer> worldPackets = new ArrayList<>();
+			
+			List<Chunk> chunks = new ArrayList<>();
+			for (ObjectCursor<ChunkImpl> cursor : world.getChunks().values()) {
+				chunks.add(cursor.value);
 			}
-		}
-		breaks.add(chunks.size());
-		
-		List<List<Chunk>> splitted = new ArrayList<>();
-		int currentBatch = 0;
-		for (int i = 0; i < chunks.size(); i++) {
-			if (breaks.contains(i)) currentBatch++;
-			if (splitted.size() <= currentBatch) {
-				splitted.add(new ArrayList<>());
+			
+			boolean skylight = Sagittarius.getInstance().getConfig().getDimension() == Dimension.OVERWORLD;
+			
+			// We try to send as few packets as possible.
+			// if we do end up with a packet that is too big, then we're sending the next chunks in a separate packet.
+			List<Integer> breaks = new ArrayList<>();
+			int totalSize = 0;
+			for (int i = 0; i < chunks.size(); i++) {
+				Chunk chunk = chunks.get(i);
+				totalSize += chunk.getSizeEstimate((short) chunk.calculatePrimaryBitMask(), skylight, true);
+				if (totalSize > 1_900_000) {
+					breaks.add(i);
+					totalSize = 0;
+				}
 			}
-			List<Chunk> chunkList = splitted.get(currentBatch);
-			chunkList.add(chunks.get(i));
-		}
-
-        for (List<Chunk> batch : splitted) {
-            ServerMapChunkBulkPacket packet = new ServerMapChunkBulkPacket(skylight, batch);
-            worldPackets.add(new PacketContainer(packet));
-        }
-
-        for (BlockMetadata metadata : world.getMetadata()) {
-			worldPackets.add(new PacketContainer(metadata.toPacket()));
-		}
-
-		this.worldPackets = worldPackets;
+			breaks.add(chunks.size());
+			
+			List<List<Chunk>> splitted = new ArrayList<>();
+			int currentBatch = 0;
+			for (int i = 0; i < chunks.size(); i++) {
+				if (breaks.contains(i)) currentBatch++;
+				if (splitted.size() <= currentBatch) {
+					splitted.add(new ArrayList<>());
+				}
+				List<Chunk> chunkList = splitted.get(currentBatch);
+				chunkList.add(chunks.get(i));
+			}
+			
+			for (List<Chunk> batch : splitted) {
+				ServerMapChunkBulkPacket packet = new ServerMapChunkBulkPacket(skylight, batch);
+				worldPackets.add(new PacketContainer(packet));
+			}
+			
+			for (BlockMetadata metadata : world.getMetadata()) {
+				worldPackets.add(new PacketContainer(metadata.toPacket()));
+			}
+			
+			this.worldPackets.complete(worldPackets);
+		});
 	}
 	
 	public void send(PlayerImpl player) {
-		for (PacketContainer container : worldPackets) {
-			player.sendPacket(container);
-		}
+		worldPackets.whenComplete((world, ex) -> {
+			if (ex != null) return;			
+			for (PacketContainer container : world) {
+				player.sendPacket(container);
+			}
+			player.sendPacket(SagittariusImpl.getInstance().getPacketCache().getPositionAndLook());
+		});
 	}
 	
 }
