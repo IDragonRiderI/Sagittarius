ac289ba7467a39f68f6ad2e41d40bf76677aa1ac
Alexander <alex@pluoi.com>
2024-04-26 16:37:56 +0200

    Update to 1.20.5

--- sagittarius-viaversion/src/main/java/de/spacepotato/sagittarius/viaversion/platform/SagittariusViaApi.java
+++ sagittarius-viaversion/src/main/java/de/spacepotato/sagittarius/viaversion/platform/SagittariusViaApi.java
@@ -1,6 +1,8 @@
 package de.spacepotato.sagittarius.viaversion.platform;
 
 import com.viaversion.viaversion.ViaAPIBase;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import com.viaversion.viaversion.api.protocol.version.VersionType;
 import de.spacepotato.sagittarius.entity.Player;
 import de.spacepotato.sagittarius.network.protocol.PacketReceiver;
 import io.netty.buffer.ByteBuf;
@@ -10,6 +12,11 @@ public class SagittariusViaApi extends ViaAPIBase<Player> {
 	@Override
 	public int getPlayerVersion(Player player) {
 		return getPlayerVersion(player.getUUID());
+	}
+
+	@Override
+	public ProtocolVersion getPlayerProtocolVersion(Player player) {
+		return ProtocolVersion.getProtocol(VersionType.RELEASE, getPlayerVersion(player));
 	}
 
 	@Override
